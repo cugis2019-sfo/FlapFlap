ee2d0196564c40330e166cb4e149d3d69a4f78e7
RavenCoffer <51681179+RavenCoffer@users.noreply.github.com>
2019-06-11 15:28:27 -0700

    Add files via upload

---
+++ day_2_(2).py
@@ -0,0 +1,72 @@
+'''
+
+5/8 #division
+
+print ("hello world, I'm learning to code today") #print
+
+print("hello", 5, "world")
+
+5/2 #division
+5-2 #subtraction
+5*2 #multiplication
+5**2 #squared
+5+2 #addition
+(8/9)*3 #parentheses
+
+
+#writing code to sum any 2 numbers
+def sum(a,b,c):
+    sum = a+b+c
+    
+    print("The sum of", a, "and", b , "and", c, "is equal to ", sum)
+
+sum(5,6,7)
+
+
+
+def triAr(a,b):
+    triAr = a*b*0.5
+    
+    print("The area of a triangle with a base of", a, "and a height of", b, "is equal to", triAr)
+
+triAr(3,4)
+
+
+
+def greet(x):
+    
+    print("Hello", x, "! Have a great day!")
+    
+greet("Molly")
+
+
+
+
+
+'''
+import math
+dir(math)
+
+
+
+def Crt(x):
+    Crt = x**(1/3)
+    print(Crt)
+    
+Crt(8)
+
+
+
+
+
+
+def askuser():
+    print("What is the value of t?")
+    
+askuser() 
+
+t = 8
+
+Crt(t)
+
+   
